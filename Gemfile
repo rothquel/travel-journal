7d22b5d15b5c109a6abc292f3f6ed192eec2ea31
Nic Rothquel <nrothquel@gmail.com>
2022-08-21 18:31:10 -0400

    created experience page

--- Gemfile
+++ Gemfile
@@ -54,6 +54,9 @@ gem "cloudinary"
 # geocoder
 gem "geocoder"
 
+# country select
+gem 'country_select'
+
 # Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
 # gem "image_processing", "~> 1.2"
 
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
 
   def index
-    @posts = Post.all
+    @posts = Post.where(user_id: current_user.id)
     @markers = @posts.geocoded.map do |post|
       {
         lat: post.latitude,
