ec34d5275085045b117bdbd81319fb7a1ed6de3c
Nic Rothquel <nrothquel@gmail.com>
2022-08-21 17:36:30 -0400

    got navigation on map working

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -1,5 +1,6 @@
 // app/javascript/controllers/map_controller.js
 import { Controller } from "@hotwired/stimulus"
+import MapboxGeocoder from "@mapbox/mapbox-gl-geocoder"
 
 export default class extends Controller {
   static values = {
