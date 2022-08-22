10fd7727a685f59c79275d45e039b94199ede88d
Nic Rothquel <nrothquel@gmail.com>
2022-08-21 21:03:30 -0400

    model really created this time

--- db/migrate/20220822005748_add_trip_foreign_key_to_user_table.rb
+++ db/migrate/20220822005748_add_trip_foreign_key_to_user_table.rb
@@ -1,7 +1,4 @@
 class AddTripForeignKeyToUserTable < ActiveRecord::Migration[7.0]
   def change
-    def change
-      add_reference :posts, :trip, foreign_key: true
-    end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_22_005748) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_22_010044) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -53,6 +53,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_22_005748) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "trip_id"
+    t.index ["trip_id"], name: "index_posts_on_trip_id"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
@@ -81,6 +83,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_22_005748) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "posts", "trips"
   add_foreign_key "posts", "users"
   add_foreign_key "trips", "users"
 end
